c37e9559ddaca859146165c061276b1624a0369a
Eric Backhage <eric.backhage@gmail.com>
2019-03-17 11:24:33 +0100

    Removed reduntant tests

--- Refactoring.Test/StatementTests.cs
+++ Refactoring.Test/StatementTests.cs
@@ -8,27 +8,6 @@ namespace Refactoring.Test
     public class StatementTests
     {
         [Test]
-        public void CreatePlay()
-        {
-            var play = new Play("hamlet", "Hamlet", "tragedy");
-            Assert.AreEqual("hamlet", play.PlayId);
-            Assert.AreEqual("Hamlet", play.Name);
-            Assert.AreEqual("tragedy", play.Type);
-        }
-
-        [Test]
-        public void CreateInvoice()
-        {
-            var invoice = new Invoice("BigCo");
-            invoice.Add("hamlet", 55);
-
-            Assert.AreEqual("BigCo", invoice.Customer);
-            Assert.AreEqual(1, invoice.Performances.Count);
-            Assert.AreEqual("hamlet", invoice.Performances[0].PlayId);
-            Assert.AreEqual(55, invoice.Performances[0].Audience);
-        }
-
-        [Test]
         public void CreateStatement()
         {
             var plays = new List<Play>
