178a2725d8f392e189e8cfc955f36e2d278bd929
Eric Backhage <eric.backhage@gmail.com>
2019-03-17 13:46:41 +0100

    Moved CreateStatementData to its own class.

--- Refactoring/StatementBuilder.cs
+++ Refactoring/StatementBuilder.cs
@@ -2,106 +2,15 @@
 using System;
 using System.Collections.Generic;
 using System.Globalization;
-using System.Linq;
+using static Refactoring.StatementDataCreation;
 
 namespace Refactoring
 {
-    public class StatementBuilder
+    public static class StatementBuilder
     {
         public static string Statement(Invoice invoice, IEnumerable<Play> plays)
         {
             return RenderPlainText(CreateStatementData(invoice, plays));
-        }
-
-        private static StatementData CreateStatementData(Invoice invoice, IEnumerable<Play> plays)
-        {
-            var statementData = new StatementData();
-            statementData.Customer = invoice.Customer;
-            statementData.Performances = invoice.Performances.Select(EnrichPerformance);
-            statementData.TotalAmount = TotalAmount(statementData);
-            statementData.TotalVolumeCredits = TotalVolumeCredits(statementData);
-
-            EnrichedPerformance EnrichPerformance(Invoice.Performance performance)
-            {
-                var result = new EnrichedPerformance(performance);
-                result.Play = PlayFor(result);
-                result.Amount = AmountFor(result);
-                result.VolumeCredits = VolumeCreditsFor(result);
-                return result;
-            }
-
-            Play PlayFor(Invoice.Performance aPerformance)
-            {
-                return plays.Single(p => p.PlayId == aPerformance.PlayId);
-            }
-
-            decimal AmountFor(EnrichedPerformance aPerformance)
-            {
-                decimal amount;
-                switch (aPerformance.Play.Type)
-                {
-                    case "tragedy":
-                        amount = 40_000m;
-                        if (aPerformance.Audience > 30)
-                        {
-                            amount += 1_000 * (aPerformance.Audience - 30);
-                        }
-                        break;
-                    case "comedy":
-                        amount = 30_000m;
-                        if (aPerformance.Audience > 20)
-                        {
-                            amount += 10_000 + 500 * (aPerformance.Audience - 20);
-                        }
-                        amount += 300 * aPerformance.Audience;
-                        break;
-                    default:
-                        throw new ArgumentException($"unknown type: {aPerformance.Play.Type}");
-                }
-
-                return amount;
-            }
-
-            int VolumeCreditsFor(EnrichedPerformance aPerformance)
-            {
-                var credits = 0;
-                credits += Math.Max(aPerformance.Audience - 30, 0);
-                if ("comedy" == aPerformance.Play.Type) credits += aPerformance.Audience / 5;
-                return credits;
-            }
-
-            decimal TotalAmount(StatementData data)
-            {
-                return data.Performances.Sum(p => p.Amount);
-            }
-
-            int TotalVolumeCredits(StatementData data)
-            {
-                return data.Performances.Sum(p => p.VolumeCredits);
-            }
-
-            return statementData;
-        }
-
-
-        private class StatementData
-        {
-            public string Customer { get; set; }
-            public IEnumerable<EnrichedPerformance> Performances { get; set; }
-            public decimal TotalAmount { get; set; }
-            public int TotalVolumeCredits { get; set; }
-        }
-
-        private class EnrichedPerformance : Invoice.Performance
-        {
-            public Play Play { get; set; }
-            public decimal Amount { get; set; }
-            public int VolumeCredits { get; set; }
-
-            public EnrichedPerformance(Invoice.Performance performance)
-                : base(performance.PlayId, performance.Audience)
-            {
-            }
         }
 
         private static string RenderPlainText(StatementData data)
---
+++ Refactoring/StatementDataCreation.cs
@@ -0,0 +1,100 @@
+﻿using Refactoring.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Refactoring
+{
+    internal static class StatementDataCreation
+    {
+        public static StatementData CreateStatementData(Invoice invoice, IEnumerable<Play> plays)
+        {
+            var statementData = new StatementData();
+            statementData.Customer = invoice.Customer;
+            statementData.Performances = invoice.Performances.Select(EnrichPerformance);
+            statementData.TotalAmount = TotalAmount(statementData);
+            statementData.TotalVolumeCredits = TotalVolumeCredits(statementData);
+
+            EnrichedPerformance EnrichPerformance(Invoice.Performance performance)
+            {
+                var result = new EnrichedPerformance(performance);
+                result.Play = PlayFor(result);
+                result.Amount = AmountFor(result);
+                result.VolumeCredits = VolumeCreditsFor(result);
+                return result;
+            }
+
+            Play PlayFor(Invoice.Performance aPerformance)
+            {
+                return plays.Single(p => p.PlayId == aPerformance.PlayId);
+            }
+
+            decimal AmountFor(EnrichedPerformance aPerformance)
+            {
+                decimal amount;
+                switch (aPerformance.Play.Type)
+                {
+                    case "tragedy":
+                        amount = 40_000m;
+                        if (aPerformance.Audience > 30)
+                        {
+                            amount += 1_000 * (aPerformance.Audience - 30);
+                        }
+                        break;
+                    case "comedy":
+                        amount = 30_000m;
+                        if (aPerformance.Audience > 20)
+                        {
+                            amount += 10_000 + 500 * (aPerformance.Audience - 20);
+                        }
+                        amount += 300 * aPerformance.Audience;
+                        break;
+                    default:
+                        throw new ArgumentException($"unknown type: {aPerformance.Play.Type}");
+                }
+
+                return amount;
+            }
+
+            int VolumeCreditsFor(EnrichedPerformance aPerformance)
+            {
+                var credits = 0;
+                credits += Math.Max(aPerformance.Audience - 30, 0);
+                if ("comedy" == aPerformance.Play.Type) credits += aPerformance.Audience / 5;
+                return credits;
+            }
+
+            decimal TotalAmount(StatementData data)
+            {
+                return data.Performances.Sum(p => p.Amount);
+            }
+
+            int TotalVolumeCredits(StatementData data)
+            {
+                return data.Performances.Sum(p => p.VolumeCredits);
+            }
+
+            return statementData;
+        }
+
+        internal class StatementData
+        {
+            public string Customer { get; set; }
+            public IEnumerable<EnrichedPerformance> Performances { get; set; }
+            public decimal TotalAmount { get; set; }
+            public int TotalVolumeCredits { get; set; }
+        }
+
+        internal class EnrichedPerformance : Invoice.Performance
+        {
+            public Play Play { get; set; }
+            public decimal Amount { get; set; }
+            public int VolumeCredits { get; set; }
+
+            public EnrichedPerformance(Invoice.Performance performance)
+                : base(performance.PlayId, performance.Audience)
+            {
+            }
+        }
+    }
+}
